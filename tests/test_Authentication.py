12e383ab23f0775001222532da39e713ccc51a10
Abbe98 <albin.post@gmail.com>
2018-09-16 10:21:17 +0200

    clean up whitespace

--- tests/test_Authentication.py
+++ tests/test_Authentication.py
@@ -11,17 +11,17 @@ def test_init_authentication():
 def test_bad_api_key():
     with pytest.raises(ValueError) as excinfo:
         auth = Authentication('hejsan-hoppsan-45fg')
-    
+
     assert 'Bad API key or inaccessible endpoint.' in str(excinfo.value)
 
 @pytest.mark.online
 def test_bad_endpoint():
     with pytest.raises(ValueError) as excinfo:
         auth = Authentication('test', endpoint='http://example.com')
-    
+
     assert 'Bad API key or inaccessible endpoint.' in str(excinfo.value)
 
     with pytest.raises(ValueError) as excinfo:
         auth = Authentication('test', endpoint='not-even-an-URL')
-    
+
     assert 'Bad API key or inaccessible endpoint.' in str(excinfo.value)
